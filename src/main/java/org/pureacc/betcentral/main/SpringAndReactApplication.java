e251d1be15268acd8abc7f507213e805e5ccfe3f
jaron <jaroncouvreur@hotmail.com>
2019-04-13 21:47:08 +0200

    Initial commit

---
+++ src/main/java/org/pureacc/betcentral/main/SpringAndReactApplication.java
@@ -0,0 +1,11 @@
+package org.pureacc.betcentral.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndReactApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndReactApplication.class, args);
+	}
+}
